3510840e846afefa55f07839c7e64966c9ca06fc
user <user@localhost.localdomain>
2023-12-07 14:02:22 -0500

    update depicated commands

--- mysql/docker-entrypoint-initdb.d/masterslave.sh
+++ mysql/docker-entrypoint-initdb.d/masterslave.sh
@@ -7,11 +7,10 @@ if [ $MYSQL_ROLE = "master" ]; then
 
     echo "Creating replication user"
     mysql -uroot -p$MYSQL_ROOT_PASSWORD \
-    --execute="CREATE USER 'replication_user'@'%' IDENTIFIED WITH mysql_native_password BY 'bigs3cret'; \
-    GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%'; FLUSH PRIVILEGES;"
+    --execute="CREATE USER 'replication_user'@'%' IDENTIFIED WITH caching_sha2_password BY 'bigs3cret'; GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%'; FLUSH PRIVILEGES;"
 
     echo "Performing temporary backup of all databases"
-    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases --master-data > /tmp/mysql-backup/master_initialization.sql
+    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases --source-data > /tmp/mysql-backup/master_initialization.sql
 
 else
 
@@ -19,16 +18,15 @@ else
     cp /etc/default/template/mysql-conf.d/slave.cnf /etc/mysql/conf.d/
 
     mysql -uroot -p$MYSQL_ROOT_PASSWORD \
-    --execute="CHANGE MASTER TO \
-                    MASTER_HOST='ghost-db-master', \
-                    MASTER_USER='replication_user', \
-                    MASTER_PASSWORD='bigs3cret';"
+    --execute="CHANGE SOURCE TO \
+                    SOURCE_HOST='ghost-db-master', \
+                    SOURCE_USER='replication_user', \
+                    SOURCE_PASSWORD='bigs3cret';"
 
     mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/mysql-backup/master_initialization.sql
 
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD --execute="START SLAVE;"
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD --execute="START REPLICA;"
 
 fi
     
-
 
