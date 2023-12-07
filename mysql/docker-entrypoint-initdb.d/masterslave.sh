58cb7404f264803a5ace25941c06d26b318c4c54
user <user@localhost.localdomain>
2023-12-07 13:34:21 -0500

    fix master slave database config strucutre:

--- mysql/docker-entrypoint-initdb.d/masterslave.sh
+++ mysql/docker-entrypoint-initdb.d/masterslave.sh
@@ -5,25 +5,30 @@ if [ $MYSQL_ROLE = "master" ]; then
     echo "MYSQL_ROLE set to $MYSQL_ROLE"
     cp /etc/default/template/mysql-conf.d/master.cnf /etc/mysql/conf.d/
 
+    echo "Creating replication user"
     mysql -uroot -p$MYSQL_ROOT_PASSWORD \
-    --execute="CREATE USER 'replication_user'@'%' IDENTIFIED BY 'bigs3cret'; \
-    GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%'; FLUSH PRIVILEGES; \
-    FLUSH TABLES WITH READ LOCK;"
+    --execute="CREATE USER 'replication_user'@'%' IDENTIFIED WITH mysql_native_password BY 'bigs3cret'; \
+    GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%'; FLUSH PRIVILEGES;"
+
+    echo "Performing temporary backup of all databases"
+    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases --master-data > /tmp/mysql-backup/master_initialization.sql
 
 else
 
     echo "MYSQL_ROLE set to slave"
     cp /etc/default/template/mysql-conf.d/slave.cnf /etc/mysql/conf.d/
 
- # CHANGE MASTER TO
- # MASTER_HOST='ghost-db-master',
- # MASTER_USER='replication_user',
- # MASTER_PASSWORD='bigs3cret',
- # MASTER_PORT=3306,
- # MASTER_LOG_FILE='master1-bin.000096',
- # MASTER_LOG_POS=157,
- # MASTER_CONNECT_RETRY=10;
- # START SLAVE;
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD \
+    --execute="CHANGE MASTER TO \
+                    MASTER_HOST='ghost-db-master', \
+                    MASTER_USER='replication_user', \
+                    MASTER_PASSWORD='bigs3cret';"
+
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/mysql-backup/master_initialization.sql
+
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD --execute="START SLAVE;"
 
 fi
     
+
+
